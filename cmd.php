c7fbfbeee1535bc6649ce6c8d9318d32f77b5312
Pedro <pedrocch@fanbot.me>
2016-06-21 13:16:38 -0500

    *

--- cmd.php
+++ cmd.php
@@ -1,18 +1,12 @@
 <?php
-require 'vendor/autoload.php';
-
 use Ratchet\Server\IoServer;
-use Ratchet\Http\HttpServer;
-use Ratchet\WebSocket\WsServer;
-use ChatApp\Chat;
+use MyApp\Chat;
 
-$server = IoServer::factory(
-    new HttpServer(
-        new WsServer(
-            new Chat()
-        )
-    ),
-    8080
-);
+    require dirname(__DIR__) . '/vendor/autoload.php';
 
-$server->run();
+    $server = IoServer::factory(
+        new Chat(),
+        8080
+    );
+
+    $server->run();
