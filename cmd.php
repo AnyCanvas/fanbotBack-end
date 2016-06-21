dc025c1c902b02ce3cae964f958f2a4b5e029aa8
Pedro <pedrocch@fanbot.me>
2016-06-21 14:25:41 -0500

    *

--- cmd.php
+++ cmd.php
@@ -6,19 +6,13 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use ChatApp\Chat;
 
-$loop   = React\EventLoop\Factory::create();
-$webSock = new React\Socket\Server($loop);
-$webSock->listen(8080, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
-
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
-	        new WampServer(
             new Chat()
-            )
         )
     ),
-    $webSock
+    8080
 );
 
 $server->run();    
