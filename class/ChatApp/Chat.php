690250b91e6ae014a538d76b228233600bb2cd1f
Pedro <pedrocch@fanbot.me>
2016-06-21 13:03:29 -0500

    *

--- class/ChatApp/Chat.php
+++ class/ChatApp/Chat.php
@@ -5,37 +5,42 @@ use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
+    protected $clients;
 
-protected $clients;
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
 
-public function __construct() {
-    $this->clients = new \SplObjectStorage;
-}
+    public function onOpen(ConnectionInterface $conn) {
+        // Store the new connection to send messages to later
+        $this->clients->attach($conn);
 
-public function onOpen(ConnectionInterface $conn) {
-    //store the new connection
-    $this->clients->attach($conn);
+        echo "New connection! ({$conn->resourceId})\n";
+    }
 
-    echo "someone connected\n";
-}
+    public function onMessage(ConnectionInterface $from, $msg) {
+        $numRecv = count($this->clients) - 1;
+        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
 
-public function onMessage(ConnectionInterface $from, $msg) {
-    //send the message to all the other clients except the one who sent.
-    foreach ($this->clients as $client) {
-        if ($from !== $client) {
-            $client->send($msg);
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                // The sender is not the receiver, send to each client connected
+                $client->send($msg);
+            }
         }
     }
-}
 
-public function onClose(ConnectionInterface $conn) {
-    $this->clients->detach($conn);
-    echo "someone has disconnected";
-}
+    public function onClose(ConnectionInterface $conn) {
+        // The connection is closed, remove it, as we can no longer send it messages
+        $this->clients->detach($conn);
 
-public function onError(ConnectionInterface $conn, \Exception $e) {
-    echo "An error has occurred: {$e->getMessage()}\n";
-    $conn->close();
-}
+        echo "Connection {$conn->resourceId} has disconnected\n";
+    }
 
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "An error has occurred: {$e->getMessage()}\n";
+
+        $conn->close();
+    }
 }
