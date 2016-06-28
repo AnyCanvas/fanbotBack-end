8c6cd8cc67ea857e8b870e5ac0be10fe30494423
Pedro <pedrocch@fanbot.me>
2016-06-28 12:02:55 -0500

    *

--- class/ChatApp/Chat.php
+++ class/ChatApp/Chat.php
@@ -10,20 +10,10 @@ class Chat implements MessageComponentInterface {
         $this->clients = new \SplObjectStorage;
     }
 
-    public function onOpen(ConnectionInterface $conn) {
+    public function onOpen(ConnectionInterface $conn, ConnectionInterface $from) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
-
         echo "New connection! ({$conn->resourceId})\n";
-        
-        $connInt = (int)$conn->resourceId;
-
-        foreach ($this->clients as $client) {
-            if ( $connInt == $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
-            }
-        }
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
@@ -42,16 +32,13 @@ class Chat implements MessageComponentInterface {
     public function onClose(ConnectionInterface $conn) {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
-
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
         echo "An error has occurred: {$e->getMessage()}\n";
-
         $conn->close();
     }
-    
 
 	public function send($client, $msg){
 	        $this->say("> ".$msg);
