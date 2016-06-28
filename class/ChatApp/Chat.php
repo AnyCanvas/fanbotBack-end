a011e7b6458c7a1914190e2a74765380216be6aa
Pedro <pedrocch@fanbot.me>
2016-06-28 12:05:25 -0500

    *

--- class/ChatApp/Chat.php
+++ class/ChatApp/Chat.php
@@ -10,7 +10,7 @@ class Chat implements MessageComponentInterface {
         $this->clients = new \SplObjectStorage;
     }
 
-    public function onOpen(ConnectionInterface $conn, $from) {
+    public function onOpen(ConnectionInterface $conn) {
         // Store the new connection to send messages to later
         $this->clients->attach($conn);
         echo "New connection! ({$conn->resourceId})\n";
