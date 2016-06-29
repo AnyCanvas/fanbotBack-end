aab0ac4a108899879ba62f17c2be44609a92626e
Pedro <pedrocch@fanbot.me>
2016-06-28 22:43:20 -0500

    *

--- hook.php
+++ hook.php
@@ -1,8 +1,7 @@
 <?php
 use WAMP\WAMPClient;
-$client = new WAMP(
-			new WAMPClient('http://localhost:8080')
-		);
+$client = new WAMPClient('http://localhost:8080');
+
 $sessionId = $client->connect();
 $client->disconnect();
 
