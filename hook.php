d6541ecc7fdf4d04b66a132e4c19670c17bce4b3
Pedro <pedrocch@fanbot.me>
2016-06-28 23:24:17 -0500

    *

--- hook.php
+++ hook.php
@@ -1,10 +1,12 @@
 <?php
 require('vendor/autoload.php');
 
+$body = @file_get_contents('php://input');
+$data = json_decode($body,true);
 use WebSocket\Client;
 
 $client = new Client("ws://104.236.71.12:8080");
-$client->send("Hello WebSocket.org!");
+$client->send($data);
 
 // echo $client->receive(); // Will output 'Hello WebSocket.org!'
 ?>
